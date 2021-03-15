37d779575d4c28665da7f9cd676b0866989f4f39
Geta Bugnar <bugnargeta@gmail.com>
2021-03-15 11:29:02 -0700

    added product and application.properties

---
+++ src/main/java/org/fasttrackit/online_shop/domain/Product.java
@@ -0,0 +1,60 @@
+package org.fasttrackit.online_shop.domain;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    private long id;
+    @NotNull
+    private String name;
+    private String description;
+    @NotNull
+    private double price;
+    @NotNull
+    private int quantity;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
