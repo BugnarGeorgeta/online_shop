95d5481c1aae407e3d09105c07a7fbc9debaeda3
Geta Bugnar <bugnargeta@gmail.com>
2021-03-19 13:05:44 -0700

    Query derived from method name

--- src/main/java/org/fasttrackit/online_shop/persistance/ProductRepository.java
+++ src/main/java/org/fasttrackit/online_shop/persistance/ProductRepository.java
@@ -1,7 +1,16 @@
 package org.fasttrackit.online_shop.persistance;
 
 import org.fasttrackit.online_shop.domain.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product>findByNameContaining(String partialName, Pageable pageable);
+
+    Page<Product>findByNameContainingAndQuantityGreaterThanEqual
+            (String partialName, int minQuantity, Pageable pageable);
+
+
 }
